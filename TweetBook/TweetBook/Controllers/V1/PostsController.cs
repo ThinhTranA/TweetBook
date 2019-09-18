a749d77b8e1b8ace25231074d6568c4123bbc806
Thinh Tran <thinhtran@outlook.com.au>
2019-09-18 18:10:32 +0800

    Creating resources using POST

--- TweetBook/TweetBook/Controllers/V1/PostsController.cs
+++ TweetBook/TweetBook/Controllers/V1/PostsController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TweetBook.Contracts.V1;
+using TweetBook.Contracts.V1.Requests;
+using TweetBook.Contracts.V1.Responses;
 using TweetBook.Domain;
 
 namespace TweetBook.Controllers.V1
@@ -25,6 +27,26 @@ namespace TweetBook.Controllers.V1
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest) //use Domain object for now
+        {
+            //This is to avoid mixed up Contract with Domain object, to help with backward compability in versioning
+            var post = new Post { Id = postRequest.Id };
+
+            if (string.IsNullOrEmpty(post.Id))
+                post.Id = Guid.NewGuid().ToString();
+
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+            var locationUri = baseUrl + "/" + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+
+            var response = new PostResponse { Id = post.Id };
+            //REST: return "post" object is nice to have.
+            //Return "locationUri is should have, so user can retrieve it. 
+            return Created(locationUri, response);
         }
     }
 }
