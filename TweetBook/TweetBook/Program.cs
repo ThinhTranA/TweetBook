4669b83759205e68a9a75242d7c9651cadfa99d1
Thinh Tran <thinhtran@outlook.com.au>
2019-09-24 21:27:54 +0800

    User specific content with JWT claims  ,automatic migrate on new migration changes

--- TweetBook/TweetBook/Program.cs
+++ TweetBook/TweetBook/Program.cs
@@ -5,16 +5,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TweetBook.Data;
 
 namespace TweetBook
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using(var servicescope = host.Services.CreateScope())
+            {
+                var dbContext = servicescope.ServiceProvider.GetRequiredService<DataContext>();
+                await dbContext.Database.MigrateAsync();  //automatic migrate on new migration changes
+            }
+
+            await host.RunAsync();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
